593d84f8f75ff7957e859d05b953a7aa0bacb5b6
Nicola Sangalli <nicolasangalli93@gmail.com>
2022-04-29 10:12:51 +0200

    Character controller test assets set up

---
+++ Assets/Test/NewCharController/Spawn.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Instantiate(player, new Vector3(10f, 0.5f, 10f), Quaternion.identity);
+    }
+
+}
