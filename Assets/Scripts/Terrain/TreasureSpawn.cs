03b175f553d2604124a2c533dc9712cbf0d6c65f
FreddyGitHM <alfredorusso71@gmail.com>
2022-05-03 13:52:47 +0200

    Commit changes

--- Assets/Scripts/Terrain/TreasureSpawn.cs
+++ Assets/Scripts/Terrain/TreasureSpawn.cs
@@ -16,6 +16,15 @@ public class TreasureSpawn : MonoBehaviour
     private int y = 0;
     private float[,] heightmap;
 
+    // Spawn distance from tree map
+    public int distance;
+
+    // Chest prefab
+    public GameObject treasure;
+
+    // Rotation in order to rotate the chest according with point normal
+    private Quaternion fitTerrain;
+
     // OnDrawGizmos
     private bool start = false;
     private Vector2 treasurePosition;
@@ -44,23 +53,48 @@ public class TreasureSpawn : MonoBehaviour
 
         //Initialize heighmap
         heightmap = td.GetHeights(0, 0, x, y);
+
+        start = true;
     }
 
-    private void calculatePosition()
+    public Vector3 getTreasurePosition()
     {
-        // Random point inside terrain
-        treasurePosition = new Vector2(Random.Range(0, x), Random.Range(0, y));
-
         // TreeMap position 
         Vector2 treeMapPos = new Vector2(RandomTreeMapGenerator.TreeWithMapPosition.x, RandomTreeMapGenerator.TreeWithMapPosition.z);
 
+        // Random point on terrain
+        treasurePosition = new Vector2(Random.Range(10, x - 10), Random.Range(10, y - 10));
+
         // 
-        while (Vector2.Distance(treasurePosition, treeMapPos) < 50)
+        while (Vector2.Distance(treasurePosition, treeMapPos) < distance)
         {
-            treasurePosition = new Vector2(Random.Range(0, x), Random.Range(0, y));
+            treasurePosition = new Vector2(Random.Range(10, x - 10), Random.Range(10, y - 10));
         }
 
-        Debug.Log("Treasure position: " + treasurePosition);
+        RaycastHit hit;
+
+        Physics.Raycast(new Vector3(treasurePosition.x, 300, treasurePosition.y), Vector3.down, out hit, Mathf.Infinity, LayerMask.GetMask("Terrain"));
+
+        fitTerrain = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+        Quaternion look = getTreasureRotation();
+
+        // var treasureChest = Instantiate(treasure, hit.point + Vector3.up * .7f, fitTerrain*look);
+
+        return hit.point + Vector3.up * .7f;
+
+    }
+
+    public Quaternion getTreasureRotation()
+    {
+
+        Vector3 treasurePos = new Vector3(treasurePosition.x, heightmap[(int)treasurePosition.y, (int)treasurePosition.x] * td.size.y, treasurePosition.y);
+
+        Quaternion treasureRot = Quaternion.LookRotation((RandomTreeMapGenerator.TreeWithMapPosition - treasurePos).normalized, Vector3.up);
+
+        // This combine two rotation, the one needed for fitting chest to terrain steepness and the other one in order to make chest look at treasure map
+        // fitTerrain rotation is applied first than treasureRot
+        return fitTerrain * treasureRot;
     }
 
     private void OnDrawGizmos()
