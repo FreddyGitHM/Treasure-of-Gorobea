6488cdd19e9d13495c2f8e50e3b6393dbe818249
FreddyGitHM <alfredorusso71@gmail.com>
2022-05-06 16:58:17 +0200

    Update Menu

--- Assets/Scripts/Player/EventsCall.cs
+++ Assets/Scripts/Player/EventsCall.cs
@@ -1,47 +1,10 @@
-using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using ExitGames.Client.Photon;
 using EventCodes;
-using System.Collections.Generic;
-using UnityEditor;
-
 
 public class EventsCall : MonoBehaviourPunCallbacks
 {
-    void Awake()
-    {
-        /*
-        // Searching for all objects in scene
-        List<GameObject> GetAllObjectsOnlyInScene()
-        {
-            List<GameObject> objectsInScene = new List<GameObject>();
-
-            foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
-            {
-                if (!EditorUtility.IsPersistent(go.transform.root.gameObject) && !(go.hideFlags == HideFlags.NotEditable || go.hideFlags == HideFlags.HideAndDontSave))
-                    objectsInScene.Add(go);
-            }
-
-            return objectsInScene;
-        }
-
-        // Searching for death canvas object
-        foreach (GameObject gameObject in GetAllObjectsOnlyInScene())
-        {
-            if (gameObject.name == "DeathCanvas")
-            {
-                canvas = gameObject;
-                Debug.Log("Death canvas find: " + gameObject.name);
-            }
-            else if (gameObject.name == "Main Camera")
-            {
-                mainCamera = gameObject;
-                Debug.Log("Main Camera found: " + gameObject.name);
-            }
-        }*/
-    }
-
     public void OnDeath()
     {
         int id = gameObject.GetComponent<PhotonView>().ViewID; //death player id
