536294c925db6483b098d9979eb94bc1a61afc55
FreddyGitHM <alfredorusso71@gmail.com>
2022-05-11 14:15:36 +0200

    Minimap fullscreen functionalitis
    Adding functionalities to the fullscreen map in order to zomm in and zoom out and move it around, clamping the camera in the bounds.

--- Assets/Scripts/Maps/Map.cs
+++ Assets/Scripts/Maps/Map.cs
@@ -7,67 +7,124 @@ public class Map : MonoBehaviour
     private GameObject FullscreenMapImage;
     private Canvas MapCanvas;
     private Camera MapCamera;
+    private Terrain terrain;
 
+    // Zoom parameters
+    private float MaxZoom;
+    public float MinZoom = 100f;
     public float scroolSpeed;
 
-    // Initial size of the camera
-    float CameraSize;
+    // Map movement parameter
+    private Vector3 dragOrigin;
+    private float MinMovement;
+    private float MaxMovement;
 
     private void Awake()
     {
         FullscreenMapImage = GameObject.Find("FullscreenMap");
+        player = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<NetworkManager>().GetPlayer();
+        MapCanvas = GameObject.Find("MapCanvas").GetComponent<Canvas>();
+        MapCamera = GetComponent<Camera>();
+        terrain = GameObject.FindGameObjectWithTag("Terrain").GetComponent<Terrain>();
     }
 
     private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<NetworkManager>().GetPlayer();
+        // Setting values for zoom
+        MaxZoom = MapCamera.orthographicSize;
 
-        CameraSize = GetComponent<Camera>().orthographicSize;
-        Debug.Log("Camera size: " + CameraSize);
-
-        MapCanvas = GameObject.Find("MapCanvas").GetComponent<Canvas>();
-
-        MapCamera = GetComponent<Camera>();
+        MinMovement = MapCamera.orthographicSize;
+        MaxMovement = terrain.terrainData.size.x - MapCamera.orthographicSize;
     }
 
     private void Update()
     {
+        // Close or open the map
+        OpenCloseMap();
+
+        if (MapCanvas.enabled)
+        {
+            DisableShooterInput();
+
+            // Map zoom
+            MapZoom();
+
+            MapMovement();
+        }
+        else
+        {
+            EnableShooterInput();
+        }
+    }
+
+    private void OpenCloseMap()
+    {
+        // Open or close the map
         if (Input.GetButtonDown("Back"))
         {
             MapCanvas.enabled = !MapCanvas.enabled;
-
-            // player.GetComponent<vShooterMeleeInput>().enabled = !player.GetComponent<vShooterMeleeInput>().enabled;
-
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.Confined;
         }
 
+        // If the map is close reset camera parameter
         if (!MapCanvas.enabled)
         {
-            MapCamera.orthographicSize = CameraSize;
-        }
-        else
-        {
-            if (Input.GetAxis("Mouse ScrollWheel") != 0f)
-            {
-                if (MapCamera.orthographicSize - Input.GetAxis("Mouse ScrollWheel") * scroolSpeed <= CameraSize)
-                {
-                    MapCamera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * scroolSpeed;
-                }
-
-            }
+            MapCamera.orthographicSize = MaxZoom;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 
-    void LateUpdate()
+    private void DisableShooterInput()
     {
-        // Vector3 newPosition = player.transform.position;
-        // newPosition.y = transform.position.y;
-        // transform.position = newPosition;
-
-        // transform.rotation = Quaternion.Euler(90f, player.transform.eulerAngles.y, 0f);
+        player.GetComponent<vShooterMeleeInput>().SetLockShooterInput(true);
     }
 
+    private void EnableShooterInput()
+    {
+        player.GetComponent<vShooterMeleeInput>().SetLockShooterInput(false);
+    }
 
+    private void MapZoom()
+    {
+        // Change zoom in and zoom out according to the mouse scroll wheel direction
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+        {
+            MapCamera.orthographicSize = ClampMapZoom(MapCamera.orthographicSize - Input.GetAxis("Mouse ScrollWheel") * scroolSpeed);
 
+            MinMovement = MapCamera.orthographicSize;
+            MaxMovement = terrain.terrainData.size.x - MapCamera.orthographicSize;
+
+            // Clamp the camera position in order to avoid exiting from bound
+            MapCamera.transform.position = ClampCameraPosition(MapCamera.transform.position);
+        }
+    }
+
+    private float ClampMapZoom(float zoom)
+    {
+        return Mathf.Clamp(zoom, MinZoom, MaxZoom);
+    }
+
+    private void MapMovement()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = MapCamera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 difference = dragOrigin - MapCamera.ScreenToWorldPoint(Input.mousePosition);
+            MapCamera.transform.position = ClampCameraPosition(MapCamera.transform.position + difference);
+        }
+    }
+
+    private Vector3 ClampCameraPosition(Vector3 camPosition)
+    {
+        float newX = Mathf.Clamp(camPosition.x, MinMovement, MaxMovement);
+        float newY = Mathf.Clamp(camPosition.z, MinMovement, MaxMovement);
+
+        return new Vector3(newX, camPosition.y, newY);
+    }
 }
