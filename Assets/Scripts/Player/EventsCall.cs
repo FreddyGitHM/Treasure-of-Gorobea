2bedd510cdf4d43f0f2ce7175825a5e6d4c1e942
Nicola Sangalli <nicolasangalli93@gmail.com>
2022-06-06 17:45:21 +0200

    exit crouching on sprint

--- Assets/Scripts/Player/EventsCall.cs
+++ Assets/Scripts/Player/EventsCall.cs
@@ -44,4 +44,9 @@ public class EventsCall : MonoBehaviourPunCallbacks
         PhotonNetwork.RaiseEvent(Codes.SHOT, data, raiseEventOptions, sendOptions);
     }
 
+    public void OnStartSprinting()
+    {
+        gameObject.GetComponent<vShooterMeleeInput>().IsCrouching = false;
+    }
+
 }
